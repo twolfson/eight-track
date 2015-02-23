676b9099a7db3142ac066406b9790cb49ef0cf8e
Todd Wolfson <todd@twolfson.com>
2015-02-22 23:25:36 -0600

    Successfully making a request

--- tmp.js
+++ tmp.js
@@ -3,10 +3,51 @@ var pem = require('pem');
 var https = require('https');
 var request = require('request');
 
-// Generate a certificate
-pem.createCertificate({days: 1, selfSigned: true}, function handleCertificate (err, keys) [
-  // TODO: It looks like the callback is using `err`
-  pem.getPublicKey(x509.cert, function handlePublicKey (err, publicCert) {
+// Allow self sign certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+// Generate a certificate
+pem.createCertificate({days: 1, selfSigned: true}, function handleCertificate (err, keys) {
+  // If there is an error, throw it
+  if (err) {
+    throw err;
+  }
+
+  // keys = {csr, clientKey, certificate, serviceKey}
+
+  // Generate a public key for our certificate
+  // TODO: It looks like the callback is using `err`
+  pem.getPublicKey(keys.certificate, function handlePublicKey (err, publicCertObj) {
+    // If there is an error, throw it
+    if (err) {
+      throw err;
+    }
+
+    // publicCertObj = {publicKey}
+
+    // Start an HTTPS server using our certificate
+    var server = https.createServer({
+      key: keys.serviceKey,
+      cert: keys.certificate
+    }, function handleRequest (req, res) {
+      res.end('hello');
+    });
+    server.listen(3000);
+
+    // Send a request to our server
+    request({
+      cert: keys.certificate,
+      key: keys.clientKey,
+      url: 'https://localhost:3000/'
+    }, function handleResponse (err, res, body) {
+      // If there is an error, throw it
+      if (err) {
+        throw err;
+      }
+
+      // Log our body
+      console.log(body);
+      process.exit();
+    });
   });
 });
